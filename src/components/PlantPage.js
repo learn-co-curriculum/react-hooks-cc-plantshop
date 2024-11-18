57e4aa3e6a1e3c221ef45af994adc6e47735851e
brian nyakundi <bazantyloffan@gmail.com>
2024-11-18 17:53:22 +0300

    Update PlantPage.js

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,13 +1,10 @@
 import React from "react";
-import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
-import Search from "./Search";
 
 function PlantPage() {
   return (
+    
     <main>
-      <NewPlantForm />
-      <Search />
       <PlantList />
     </main>
   );
