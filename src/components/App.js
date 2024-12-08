78b20fda166b906da496178ad306e72ffe6e7d37
Mwalimu-Kungu <jameskungu10@gmail.com>
2024-12-08 13:03:45 -0500

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Header from "./Header";
 import PlantPage from "./PlantPage";
 
-function App() {
+function HomePage() {
   return (
-    <div className="app">
-      <Header />
-      <PlantPage />
+    <div>
+      <h2>Welcome to Plantsy</h2>
+      <p>Your favorite plant shop!</p>
+      <Link to="/plants">
+        <button>Go to Plants</button>
+      </Link>
     </div>
   );
 }
 
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/plants" element={<PlantPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
 export default App;
+
